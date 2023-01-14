e478c1dd74a2cc87d4fe3f77d419b1febefe73a1
Leaftyyyy <rayonteo@gmail.com>
2023-01-14 15:15:38 +0800

    Added nodemon socketio.

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-import express from 'express';
+import express from "express";
+import http from "http";
+import socketIO from "socket.io";
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
-app.listen(3000, () => {
-    console.log('listening on port 3000');
-})
+app.get("/", (req, res) => {
+  res.send("test");
+});
+
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+  console.log(`Server is up on port: ${port}`);
+});
